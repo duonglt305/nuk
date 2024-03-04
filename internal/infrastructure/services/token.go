4557d58577b5c10904ba5e98a1c717efb10d0bc8
Duong Le <duonglt30597@gmail.com>
2024-03-04 23:03:06 +0700

    refactor: move snowflake to infrastructure

--- internal/infrastructure/services/token.go
+++ internal/infrastructure/services/token.go
@@ -1,4 +1,4 @@
-package services
+package infras_services
 
 import (
 	"fmt"
@@ -7,22 +7,26 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
+// ITokenService is an interface for token service
 type ITokenService[T any] interface {
 	Create(id T) (string, error)
 	GetID(token string) (*T, error)
 	ExtractClaims(token string) (*TokenClaims[T], error)
 }
 
+// TokenService is a service for creating and extracting tokens
 type TokenService[T any] struct {
 	secretKey []byte
 	lifetime  time.Duration
 }
 
+// TokenClaims is a struct for token claims
 type TokenClaims[T any] struct {
 	ID T `json:"id"`
 	jwt.MapClaims
 }
 
+// NewTokenService creates a new token service
 func NewTokenService[T any](
 	secretKey []byte,
 	lifetime time.Duration,
@@ -33,6 +37,7 @@ func NewTokenService[T any](
 	}
 }
 
+// Create creates a new token
 func (s *TokenService[T]) Create(id T) (string, error) {
 	now := time.Now().UTC()
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, TokenClaims[T]{
@@ -46,6 +51,7 @@ func (s *TokenService[T]) Create(id T) (string, error) {
 	return token.SignedString(s.secretKey)
 }
 
+// GetID gets the id from the token
 func (s *TokenService[T]) GetID(token string) (*T, error) {
 	claims, err := s.ExtractClaims(token)
 	if err != nil {
@@ -54,6 +60,7 @@ func (s *TokenService[T]) GetID(token string) (*T, error) {
 	return &claims.ID, nil
 }
 
+// ExtractClaims extracts the claims from the token
 func (s *TokenService[T]) ExtractClaims(token string) (*TokenClaims[T], error) {
 	claims := TokenClaims[T]{}
 	t, err := jwt.ParseWithClaims(token, &claims, func(token *jwt.Token) (any, error) {
