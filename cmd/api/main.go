073db6f8b70b530da44525b8f0f3029860e719fc
Duong Le <duonglt30597@gmail.com>
2024-04-17 22:02:06 +0700

    WIP: render html

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,26 +1,24 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	"duonglt.net/internal"
-	"github.com/spf13/viper"
+	"duonglt.net/pkg/utils"
 )
 
 func main() {
-	viper.SetConfigFile(".env")
-	if err := viper.ReadInConfig(); err != nil {
-		log.Printf("failed to read config: %+v\n", err)
-		os.Exit(1)
-	}
-	r, err := internal.Initialize()
-	if err != nil {
-		log.Printf("%+v\n", err)
-		os.Exit(1)
-	}
-	if err := r.ServeHTTP(); err != nil {
-		log.Printf("failed to start application: %+v\n", err)
-		os.Exit(1)
-	}
+	h := utils.NewHtmlParser("templates")
+	println(h.Parse("email/passwords/forgot.html"))
+	/*viper.SetConfigFile(".env")
+	  if err := viper.ReadInConfig(); err != nil {
+	  	log.Printf("failed to read config: %+v\n", err)
+	  	os.Exit(1)
+	  }
+	  r, err := internal.Initialize()
+	  if err != nil {
+	  	log.Printf("%+v\n", err)
+	  	os.Exit(1)
+	  }
+	  if err := r.ServeHTTP(); err != nil {
+	  	log.Printf("failed to start application: %+v\n", err)
+	  	os.Exit(1)
+	  }*/
 }
--- pkg/utils/html.go
+++ pkg/utils/html.go
@@ -1,18 +1,80 @@
 package utils
 
 import (
-	"bytes"
-	"html/template"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
 )
 
-func ParseHTML(name string, data map[string]any, files ...string) (*bytes.Buffer, error) {
-	t, err := template.ParseFiles(files...)
-	if err != nil {
-		return nil, err
+var (
+	rAllowedExtensions = regexp.MustCompile(`\.(html|tpl)$`)
+	rExtends           = regexp.MustCompile(`{{\s*extends\s+"([^"]+)"\s*}}`)
+	rYield             = regexp.MustCompile(`{{\s*yield\s*}}`)
+	rBlock             = regexp.MustCompile(`{{\s*block\s+"([^"]+)"\s*}}\s*(.*?)\s*{{\s*end\s*}}`)
+)
+
+type HtmlParser struct {
+	root      string
+	templates map[string]*Template
+}
+
+func NewHtmlParser(root string) *HtmlParser {
+	h := &HtmlParser{root: root, templates: make(map[string]*Template)}
+	if err := h.LoadTemplates(); err != nil {
+		log.Fatalf("failed to load templates: %+v\n", err)
 	}
-	var body bytes.Buffer
-	if err := t.ExecuteTemplate(&body, name, data); err != nil {
-		return nil, err
+	return h
+}
+
+func (p *HtmlParser) LoadTemplates() error {
+	if err := filepath.Walk(p.root, func(path string, info fs.FileInfo, err error) error {
+		if !info.IsDir() && rAllowedExtensions.MatchString(path) {
+			name, _ := filepath.Rel(p.root, path)
+			p.templates[name] = &Template{name, path, p, make(map[string]string)}
+		}
+		return err
+	}); err != nil {
+		return err
 	}
-	return &body, nil
+	return nil
+}
+func (p *HtmlParser) Parse(name string) string {
+	if _, ok := p.templates[name]; !ok {
+		log.Fatalf("template %s not found\n", name)
+	}
+	return p.templates[name].Render()
+}
+
+type Template struct {
+	name   string
+	path   string
+	parser *HtmlParser
+	blocks map[string]string
+}
+
+func (t *Template) Render() string {
+	path := t.path
+
+	for {
+		bytes, err := os.ReadFile(path)
+		if err != nil {
+			log.Fatalf("failed to read template: %+v\n", err)
+		}
+		res := rExtends.FindStringSubmatch(string(bytes))
+		if len(res) == 0 {
+			return string(bytes)
+		}
+		bytes = rExtends.ReplaceAllFunc(bytes, func(b []byte) []byte {
+			tpl := t.parser.templates[res[1]]
+			if tpl == nil {
+				log.Printf("template %s not found\n", res[1])
+				return nil
+			}
+			return []byte(tpl.Render())
+		})
+		path = filepath.Join(t.parser.root, res[1])
+	}
+	return ""
 }
